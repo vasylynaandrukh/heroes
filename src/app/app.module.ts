417ce2318d3b35bf3d30e708e28b9cf85c7f622f
VasylynaAndrukh <vasylynaandrukh@gmail.com>
2019-03-11 19:14:16 +0200

    add hero-detail.component and work!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// @ts-ignore
-import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import {FormsModule} from "@angular/forms";
+
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import {FormsModule} from "@angular/forms";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 import { MessagesComponent } from './messages/messages.component';
+
 
 @NgModule({
   declarations: [
